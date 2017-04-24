d0148826dfb4c41208d9a86368fe43270f8ba83c
JMH <jheld135@gmail.com>
2017-04-24 10:57:44 -0500

    Refactor webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,7 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" },
       { test: /\.css$/, use: ExtractTextPlugin.extract({
         use: 'css-loader'
       })
