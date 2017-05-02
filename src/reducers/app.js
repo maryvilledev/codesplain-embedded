d9bd33fe3f160805555650008dcc37a1bcd6a8dd
JMH <jheld135@gmail.com>
2017-05-02 14:30:37 -0500

    Add reducer for TOGGLE_RULE

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -16,6 +16,20 @@ const app = (state = initialState, action) => {
     case actions.SET_STATE: {
       return Object.assign({}, action.payload);
     }
+    case actions.TOGGLE_RULE: {
+      const currentFilters = state.filters;
+      const toggledRule = action.payload;
+      return {
+        ...state,
+        filters: {
+          ...currentFilters,
+          [toggledRule]: {
+            ...currentFilters[toggledRule],
+            selected: !currentFilters[toggledRule].selected,
+          },
+        },
+      };
+    }
     case actions.SET_SELECTED_LINE: {
       if (state.selectedLine === action.payload) {
         return {
